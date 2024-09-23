619150e7b893e4cdda759b7d7feadf92bebc7ea4
zhu wei <724607039@qq.com>
2024-09-23 22:55:58 +0800

    测试校验

--- commitlintrc.cjs
+++ commitlintrc.cjs
@@ -1,26 +1,23 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'perf',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'revert',
-        'config',
-        'chore',
-        'bulid',
-        'ci',
-        'Chores'
-      ]
-    ],
-    'subject-full-stop': [0, 'never'],
-    'subject-case:': [0, 'never'],
+      'type-enum': [
+          2,
+          'always',
+          [
+              'build',
+              'ci',
+              'chore',
+              'docs',
+              'feat',
+              'fix',
+              'perf',
+              'refactor',
+              'revert',
+              'style',
+              'test',
+              'addLog'
+          ]
+      ],
   },
 }
